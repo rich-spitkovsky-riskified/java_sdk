7ae88fc6ca585979a30486d7f1ccdcdce4d65700
Rich Spitkovsky <richard.spitkovsky@riskified.com>
2019-05-23 13:59:32 -0400

    added AuthenticationResult to CheckoutDenied class

--- riskified-sdk/src/main/java/com/riskified/models/CheckoutDeniedOrder.java
+++ riskified-sdk/src/main/java/com/riskified/models/CheckoutDeniedOrder.java
@@ -6,11 +6,14 @@ import com.riskified.validations.*;
 public class CheckoutDeniedOrder extends BaseOrder {
 
     private AuthorizationError authorizationError;
+    private AuthenticationResult authenticationResult;
 
     public CheckoutDeniedOrder(String id) {
     	this.id = id;
     }
-    
+
+
+//    Why do we have two CheckoutDeniedFunctions?? Is the one above deprecated? Do we need to add AuthenticationError as a parameter to the below function? 
     public CheckoutDeniedOrder(String id, AuthorizationError authorizationError) {
     	this.id = id;
         this.authorizationError = authorizationError;
@@ -23,6 +26,11 @@ public class CheckoutDeniedOrder extends BaseOrder {
     public void setAuthorizationError(AuthorizationError authorizationError) {
         this.authorizationError = authorizationError;
     }
+
+    public AuthenticationResult getAuthenticationResult() { return authenticationResult; }
+
+    public void setAuthenticationResult(AuthenticationResult authenticationResult) { this.authenticationResult = authenticationResult; }
+
 
     public void validate(Validation validationType) throws FieldBadFormatException {
 
